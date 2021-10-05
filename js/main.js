729f3c2320245c833d758378f59e3ebe39600884
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-10-05 20:54:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(function(){
-    $('.btn__menu').on('click', function(){
-        $('.menu__list').slideToggle();
+    // Слайде
+    $('.slider').slick({
+        arrows: false,
+        fade: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
     })
+
+    // Включить меню
+    $('.header-btn').on('click', function(){
+        $('.menu').addClass('menu__active');
+    });
+
+    // Выключить меню
+    $('.close-btn').on('click', function(){
+        $('.menu').removeClass('menu__active')
+    })
+    
 });
